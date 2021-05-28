4d0a6d4010d83b9554b063c97dac5d8292d2e5a9
jade-09 <81950117+jade-09@users.noreply.github.com>
2021-05-28 23:42:54 +0530

    Add files via upload

---
+++ automation.py
@@ -0,0 +1,10 @@
+# import pyautogui
+# import time
+pyautogui.size()
+pyautogui.position()
+pyautogui.position()
+for i in range (11):
+    pyautogui.click(x=391, y=398)
+    pyautogui.click(x=391, y=398)
+    pyautogui.click(x=391, y=398)
+    time.sleep(0.2)
